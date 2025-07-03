dfe16f5e082a6dfe5be05bb3cb5852743b3909be
Bipin Kumar Pandey <bipinkumar.pandey@actian.com>
2025-07-03 19:31:31 +0530

    config changes

---
+++ scripts/scripts.js
@@ -0,0 +1,102 @@
+/*
+ * Copyright 2022 Adobe. All rights reserved.
+ * This file is licensed to you under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License. You may obtain a copy
+ * of the License at http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed under
+ * the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR REPRESENTATIONS
+ * OF ANY KIND, either express or implied. See the License for the specific language
+ * governing permissions and limitations under the License.
+ */
+
+import { setLibs } from './utils.js';
+import loadProfileDetails from '../blocks/utils/profileUtils.js';
+// Add project-wide styles here.
+const STYLES = '';
+
+// Use '/libs' if your live site maps '/libs' to milo's origin.
+const LIBS = 'https://milo.adobe.com/libs';
+
+// Add any config options.
+const CONFIG = {
+  // codeRoot: '',
+  // contentRoot: '',
+  // imsClientId: 'college',
+  // geoRouting: 'off',
+  // fallbackRouting: 'off',
+  locales: {
+    '': { ietf: 'en-US', tk: 'hah7vzn.css' },
+    de: { ietf: 'de-DE', tk: 'hah7vzn.css' },
+    kr: { ietf: 'ko-KR', tk: 'zfo3ouc' },
+  },
+};
+
+// Default to loading the first image as eager.
+(async function loadLCPImage() {
+  const lcpImg = document.querySelector('img');
+  lcpImg?.setAttribute('loading', 'eager');
+}());
+
+/*
+ * ------------------------------------------------------------
+ * Edit below at your own risk
+ * ------------------------------------------------------------
+ */
+
+const miloLibs = setLibs(LIBS);
+
+(function loadAbpStyles() {
+  const path = '/styles/styles.css';
+  const link = document.createElement('link');
+  link.setAttribute('rel', 'stylesheet');
+  link.setAttribute('href', path);
+  document.head.appendChild(link);
+}());
+
+(function loadStyles() {
+  const paths = [`${miloLibs}/styles/styles.css`];
+  if (STYLES) { paths.push(STYLES); }
+  paths.forEach((path) => {
+    const link = document.createElement('link');
+    link.setAttribute('rel', 'stylesheet');
+    link.setAttribute('href', path);
+    document.head.appendChild(link);
+  });
+}());
+
+// Prevent redirection to helpx url when pressing enter in search
+(function disbaledGnavSearchEnter() {
+  EventTarget.prototype.addEventListener = new Proxy(EventTarget.prototype.addEventListener, {
+    apply: (targetFn, targetElement, argumentsList) => {
+      const [event, fn] = argumentsList;
+      const doNothing = () => { };
+      const shouldDoNothing = targetElement?.classList?.[0] === 'gnav-search-input' && event === 'keydown';
+      const args = [event, shouldDoNothing ? doNothing : fn];
+      Reflect.apply(targetFn, targetElement, args);
+    },
+  });
+}());
+
+const { loadArea, setConfig } = await import(`${miloLibs}/utils/utils.js`);
+
+(async function loadPage() {
+  setConfig({ ...CONFIG, miloLibs });
+  await loadArea();
+  await loadProfileDetails();
+}());
+
+/*
+ * utils
+ */
+
+/**
+ * Sanitizes a name for use as class name.
+ * @param {*} name The unsanitized name
+ * @returns {string} The class name
+ */
+export function toClassName(name) {
+  return name && typeof name === 'string'
+    ? name.toLowerCase().replace(/[^0-9a-z]/gi, '-')
+    : '';
+}
